6724eda8920aa00b693936be2109928119a0da54
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-09-13 00:30:29 +1000

    Comment out deprecated request and use node-fetch

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-const request = require('request'); // Import the 'request' module for making HTTP requests
+// const express = require('express');
+// const cors = require('cors');
+// const request = require('request'); // Import the 'request' module for making HTTP requests
+import express from "express";
+import cors from "cors";
+const fetch = (await import('node-fetch')).default;
 const app = express();
 
 app.use(cors())
 
 // Define a route that will handle requests to the external API
-app.get('/api/nasa', (req, res) => {
+// app.get('/api/nasa', (req, res) => {
+//
+//     const { apiUrl } = req.query;
+//     console.log(apiUrl);
+//
+//     // Forward the request to the external API
+//     request(apiUrl, { json: true }, (err, response, body) => {
+//         if (err) {
+//             return res.status(500).json({ error: 'Error fetching data from API' });
+//         }
+//         res.json(body); // Send the API response to the client
+//     });
+// });
+
+app.get('/api/nasa', async (req, res) => {
 
     const { apiUrl } = req.query;
     console.log(apiUrl);
 
-    // Forward the request to the external API
-    request(apiUrl, { json: true }, (err, response, body) => {
-        if (err) {
+    try {
+        // Fetch data
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
             return res.status(500).json({ error: 'Error fetching data from API' });
         }
-        res.json(body); // Send the API response to the client
-    });
+
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        return res.status(500).json({ error: 'Error fetching data from API' });
+    }
 });
+
 
 app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 80')
